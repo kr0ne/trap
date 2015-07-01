6df2900ead8cb47d9f2dbe5538d8ace94c429e41
Aleksandr <ogonek16@gmail.com>
2015-07-01 17:38:04 +0300

    Added simple template for displaying requests

--- trap/settings.py
+++ trap/settings.py
@@ -105,5 +105,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'trap/templates'),
 )
--- trap/views.py
+++ trap/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 import datetime
 from django.views.decorators.csrf import csrf_exempt
@@ -10,13 +11,19 @@ def requests(request):
 
 @csrf_exempt
 def homepage(request):
-    date = datetime.datetime.today()
-    a = request.get_full_path()
-    stringa = ""
-    if request.method == "POST":
-        c = dict(request.POST.lists())
-        for k,v in c.items():
-            stringa += "key=%s, value=%s\t" % (k, v[0])
-        return HttpResponse(str(date) + "\t" + a + "\t" + stringa)
-    else:
-        return HttpResponse(str(date) + "\t" + a)
+    # date = datetime.datetime.today()
+    # a = request.get_full_path()
+    # stringa = ""
+    # if request.method == "POST":
+    #     c = dict(request.POST.lists())
+    #     for k, v in c.items():
+    #         stringa += "key=%s, value=%s\t" % (k, v[0])
+    #     return HttpResponse(str(date) + "\t" + a + "\t" + stringa)
+    # else:
+    #     return HttpResponse(str(date) + "\t" + a)
+    req_array = "asdasda"
+    context = RequestContext(request, {
+        'req_array': req_array,
+    })
+    template = loader.get_template('requests.html')
+    return HttpResponse(template.render(context))
