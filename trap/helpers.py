6d65d01f63d8387575defe84d7c016b1c2f18e77
Aleksandr <ogonek16@gmail.com>
2015-07-02 15:17:04 +0300

    in-between refactoring. Added helpers-functions, fixed GET&POST query parameters

--- trap/helpers.py
+++ trap/helpers.py
@@ -5,6 +5,7 @@ from datetime import datetime
 def get_current_time():
     return datetime.now()
 
+
 def get_rem_ip_address(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -13,14 +14,18 @@ def get_rem_ip_address(request):
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
+
 def get_request_method(request):
     return request.method
+
 
 def get_request_scheme(request):
     return request.scheme
 
+
 def get_query_string(request):
     return request.META.get('QUERY_STRING')
+
 
 def get_query_parameters(request):
     method = get_request_method(request)
@@ -43,9 +48,10 @@ def get_query_parameters(request):
         pretty_string = pretty_string + get_string
     return pretty_string
 
+
 def get_cookies(request):
     return request.COOKIES
 
+
 def get_request_headers(request):
     return request.META
-
