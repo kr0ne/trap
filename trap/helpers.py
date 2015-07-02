44ddc467358ca13686416553dec8ce02746cd01f
Aleksandr <ogonek16@gmail.com>
2015-07-02 14:41:55 +0300

    in-between refactoring. Added helpers-functions, fixed GET&POST query parameters

---
+++ trap/helpers.py
@@ -0,0 +1,51 @@
+__author__ = 'sunde'
+from datetime import datetime
+
+
+def get_current_time():
+    return datetime.now()
+
+def get_rem_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_request_method(request):
+    return request.method
+
+def get_request_scheme(request):
+    return request.scheme
+
+def get_query_string(request):
+    return request.META.get('QUERY_STRING')
+
+def get_query_parameters(request):
+    method = get_request_method(request)
+    pretty_string = ""
+    if method == 'GET':
+        get_dict = request.GET
+        for k, v in get_dict.items():
+            pretty_string += "key=%s, value=%s\t" % (k, v)
+    elif method == 'POST':
+        post_dict = request.POST
+        if post_dict:
+            pretty_string += "Post parameters: "
+            for k, v in post_dict.items():
+                pretty_string += "key=%s, value=%s\t" % (k, v)
+        get_dict = request.GET
+        if get_dict:
+            get_string = "\nGet parameters:\n"
+            for j,h in get_dict.items():
+                get_string += "key=%s, value=%s\t" % (j, h[0])
+        pretty_string = pretty_string + get_string
+    return pretty_string
+
+def get_cookies(request):
+    return request.COOKIES
+
+def get_request_headers(request):
+    return request.META
+
--- trap/views.py
+++ trap/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.template import RequestContext, loader
-
+from helpers import *
 from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
@@ -11,30 +11,18 @@ def requests(request):
 
 @csrf_exempt
 def homepage(request):
-    # date = datetime.datetime.today()
-    # a = request.get_full_path()
-    stringa = ""
-    # if request.method == "POST":
-    #     c = dict(request.POST.lists())
-    #     for k, v in c.items():
-    #         stringa += "key=%s, value=%s\t" % (k, v[0])
-    #     return HttpResponse(str(date) + "\t" + a + "\t" + stringa)
-    # else:
-    #     return HttpResponse(str(date) + "\t" + a)
+
     #test limk = http://127.0.0.1:8000/some/page?value=1&foramat_field=1
-    req_array = {}
-    req_array['req_time'] = datetime.now()
-    req_array['rem_ip'] = request.META.get('REMOTE_ADDR')
-    #TODO make rem ip for all caeses. Need add check request.META.get('HTTP_X_FORWARDED_FOR').split(',')[0]
-    req_array['req_method'] = request.method
-    req_array['scheme'] = request.scheme
-    req_array['query_string'] = request.META.get('QUERY_STRING')
-    a = dict(request.GET.lists())
-    for k, v in a.items():
-        stringa += "key=%s, value=%s\t" % (k, v[0])
-    req_array['query_parameters'] = stringa
-    req_array['cookies'] = request.COOKIES
-    req_array['headers'] = request.META
+    req_array = dict()
+    req_array['req_time'] = get_current_time()#datetime.now()
+    req_array['rem_ip'] = get_rem_ip_address(request)
+    req_array['req_method'] = get_request_method(request)
+    req_array['scheme'] = get_request_scheme(request)
+    req_array['query_string'] = get_query_string(request)
+    req_array['query_parameters'] = get_query_parameters(request)
+    req_array['cookies'] = get_cookies(request)
+    req_array['headers'] = get_request_headers(request)
+
     context = RequestContext(request, {
         'req_array': req_array,
     })
